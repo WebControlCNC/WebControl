5e608856f8d12b7e5c2269c4a534253d28ccf0d6
madgrizzle <john@thehogans.name>
2019-10-13 23:58:15 +0100

    update tools

--- DataStructures/data.py
+++ DataStructures/data.py
@@ -38,7 +38,7 @@ class Data:
     Version Updater
     '''
     lastChecked = -1
-    pyInstallCurrentVersion = 0.907
+    pyInstallCurrentVersion = 0.908
     pyInstallUpdateAvailable = False
     pyInstallUpdateBrowserUrl = ""
     pyInstallUpdateVersion = 0
