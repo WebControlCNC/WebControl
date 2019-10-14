8e6547386d743dea89150b5f821e9df286af3777
madgrizzle <john@thehogans.name>
2019-10-14 03:01:07 +0100

    firmware data

--- DataStructures/data.py
+++ DataStructures/data.py
@@ -38,7 +38,7 @@ class Data:
     Version Updater
     '''
     lastChecked = -1
-    pyInstallCurrentVersion = 0.908
+    pyInstallCurrentVersion = 0.909
     pyInstallUpdateAvailable = False
     pyInstallUpdateBrowserUrl = ""
     pyInstallUpdateVersion = 0
