b47b56c6f232b1f5062c2b77bcb1ee08456315a5
John Hogan <john@thehogans.name>
2019-11-13 15:18:04 -0500

    update releases version

--- DataStructures/data.py
+++ DataStructures/data.py
@@ -38,7 +38,7 @@ class Data:
     Version Updater
     '''
     lastChecked = -1
-    pyInstallCurrentVersion = 0.914
+    pyInstallCurrentVersion = 0.915
     pyInstallUpdateAvailable = False
     pyInstallUpdateBrowserUrl = ""
     pyInstallUpdateVersion = 0
