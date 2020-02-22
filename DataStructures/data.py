f40f9125fdd3a075abe94bb394b6ea2afb30707b
madgrizzle <john@thehogans.name>
2020-02-22 15:56:23 -0500

    uptick version

--- DataStructures/data.py
+++ DataStructures/data.py
@@ -38,7 +38,7 @@ class Data:
     Version Updater
     '''
     lastChecked = -1
-    pyInstallCurrentVersion = 0.926
+    pyInstallCurrentVersion = 0.927
     pyInstallUpdateAvailable = False
     pyInstallUpdateBrowserUrl = ""
     pyInstallUpdateVersion = 0
