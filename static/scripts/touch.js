7a34fa93d5a03567dfe77a5d69f532aa8b43f339
md8n <lee@md8n.com>
2022-12-30 13:32:00 +0900

    Tidy up touch.js

--- static/scripts/touch.js
+++ static/scripts/touch.js
@@ -1,10 +1,26 @@
 import { requestPage } from "./socketEmits.js";
 
-var onlongtouch; 
 var timer;
 var touchduration = 1500; //length of time we want the user to touch before we do something
 var lastTouchEvent = null;
 
+function onlongtouch(e) { 
+    console.log("long touch detected", e.touches[0].clientX, e.touches[0].clientY);
+    // TODO: the global `event` is deprecated, this needs replacing
+    event = {
+    	clientX: e.touches[0].clientX,
+	    clientY: e.touches[0].clientY
+    }
+    var pos = cursorPosition();
+    x = pos.x;
+    x = x.toFixed(4);
+    pos.x = x;
+
+    y = pos.y;
+    y = y.toFixed(4);
+    pos.y = y;
+    requestPage("screenAction", pos);
+};
 
 function touchstart(e) {
     e.preventDefault();
@@ -14,7 +30,7 @@ function touchstart(e) {
 		    onlongtouch(lastTouchEvent);
 		    timer = null;
 		    lastTouchEvent = null;
-	       	}, touchduration);
+	    }, touchduration);
     }
 }
 
@@ -26,23 +42,6 @@ function touchend(e) {
         lastTouchEvent = null;
     }
 }
-
-onlongtouch = function(e) { 
-    console.log("long touch detected", e.touches[0].clientX, e.touches[0].clientY); 
-    event = {
-    	clientX:     e.touches[0].clientX,
-	clientY:     e.touches[0].clientY
-    }
-    var pos = cursorPosition();
-            x = pos.x;
-            x = x.toFixed(4);
-            pos.x = x;
-
-            y = pos.y;
-            y = y.toFixed(4);
-            pos.y = y;
-    requestPage("screenAction",pos)
-};
 
 document.addEventListener("DOMContentLoaded", function(event) { 
     $("#workarea")[0].addEventListener("touchstart", touchstart, false);
